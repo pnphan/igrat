e7b29bb71f474b26d46584557d280ad182ab2380
pnphan <tammy.phan@utoronto.ca>
2025-05-23 01:43:59 -0400

    docs

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+IGRA (Integrated Global Radiosonde Archive) Toolkit
+
+This library provides tools for working with IGRA radiosonde data, including:
+- Reading and parsing station metadata
+- Processing individual station data files
+- Data analysis and visualization
+- Data export and conversion
+"""
+
+__version__ = "0.1.0" 
